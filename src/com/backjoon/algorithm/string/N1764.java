87e8cb7832888ddffdcc97772c1e75ad2cdccf91
zbass90 <smilejkh90@gmail.com>
2025-10-22 23:49:25 +0900

    feat: 백준 문자열 N1764 답안 수정

--- src/com/backjoon/algorithm/string/N1764.java
+++ src/com/backjoon/algorithm/string/N1764.java
@@ -3,7 +3,11 @@ package com.backjoon.algorithm.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
@@ -35,6 +39,42 @@ public class N1764 {
 	// 이어서 보잡 사람의 순서로 주어짐
 	// 이름은 띄어쓰기 없이 알파벳 소문자, 그 길이는 20이하
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		// 듣도 못한 사람 저장용 (탐색용: HashSet)
+		Set<String> heard = new HashSet<>();
+
+		for (int i = 0; i < n; i++) {
+			heard.add(br.readLine());
+		}
+
+		// 교집합 결과 저장용
+		List<String> both = new ArrayList<>();
+
+		for (int i = 0; i < m; i++) {
+			String name = br.readLine();
+			if (heard.contains(name)) {
+				both.add(name);
+			}
+		}
+
+		// 사전순 정렬
+		Collections.sort(both);
+
+		// 출력
+		StringBuilder sb = new StringBuilder();
+		sb.append(both.size()).append('\n');
+		for (String name : both) {
+			sb.append(name).append('\n');
+		}
+
+		System.out.print(sb);
+	}
+
+	void before() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
